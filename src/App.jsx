55887b13752c579b874aa276cf0bcad39cf3a593
Mustafa-Lutaaya <cestmoimuganga@gmx.de>
2024-08-22 00:07:27 +0200

    Exercise 1.7 Step 2

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ const handleGoodClick = () => setGood(good + 1);
 const handleNeutralClick = () => setNeutral(neutral + 1);
 const handleBadClick = () => setBad(bad + 1);
 
+const totalFeedback = good + neutral + bad;
+const averageScore = totalFeedback > 0 ? (good - bad) / totalFeedback : 0;
+const positivePercentage = totalFeedback > 0 ? (good / totalFeedback) * 100 : 0;
+
 return (
   <div>
     <h1>Give feeback</h1>
@@ -20,6 +24,9 @@ return (
     <p>Good: {good}</p>
     <p>Neutral: {neutral}</p>
     <p>Bad: {bad}</p>
+    <p>Total feeback: {totalFeedback}</p>
+    <p>Average score: {averageScore.toFixed(1)}</p>
+    <p>Positive feedback: {positivePercentage.toFixed(1)}%</p>
   </div>
   );
 };
