34e7cc7c23aedc9549391e3e7b8b6dc3824e30e8
Mustafa-Lutaaya <cestmoimuganga@gmx.de>
2024-08-22 16:42:52 +0200

    Exercise 1.12 Step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,29 @@
 import { useState } from 'react';
 
-const Button = ({ onClick, text }) => {
-  return <button onClick={onClick}>{text}</button>;
-};
-
-const StatisticLine = ({ text, value }) => {
-  return ( 
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  );
-};
-
-const Statistics = ({ good, neutral, bad, totalFeedback, averageScore, positivePercentage }) => {
-  if (totalFeedback === 0) {
-    return <p>No feedback given</p>;
-  }
-  
-  return (
-    <table>
-      <tbody>
-      <StatisticLine text="Good" value={good} />
-      <StatisticLine text="Neutral" value={neutral} />
-      <StatisticLine text="Bad" value={bad} />
-      <StatisticLine text="Total feedback" value={totalFeedback} />
-      <StatisticLine text="Average score" value={averageScore.toFixed(1)} />
-      <StatisticLine text="Positive feeback" value={`${positivePercentage.toFixed(1)}%`} />
-      </tbody>
-    </table>
-  );
-};
-
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  
-  const totalFeedback = good + neutral + bad;
-  const averageScore = totalFeedback > 0 ? (good - bad) / totalFeedback : 0;
-  const positivePercentage = totalFeedback > 0 ? (good / totalFeedback) * 100 : 0;
-  
-  return (
-  <div>
-    <h1>Give feeback</h1>
-    <Button onClick={() => setGood(good + 1)} text="Good" />
-    <Button onClick={() => setNeutral(neutral + 1)} text="Neutral" />
-    <Button onClick={() => setBad(bad + 1)} text="Bad" />
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software projects makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, no smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ];
 
+  const [selected, setSelected] = useState(0);
 
-    <h2>Statistics</h2>
-    <Statistics
-    good={good}
-    neutral={neutral}
-    bad={bad}
-    totalFeedback={totalFeedback}
-    averageScore={averageScore}
-    positivePercentage={positivePercentage}
-    />
+  const getRandomAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
+  };
+
+  return(
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={getRandomAnecdote}>Next Anecdote</button>
     </div>
   );
 };
-
 export default App; 
